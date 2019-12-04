9dbc79e7557c5007756ab6c1cc2a7879a5f41ae5
csyhping <csyhping@connect.hku.hk>
2019-12-04 10:22:20 +0800

    Day 1 part 2

--- Day_1_The_Tyranny_of_the_Rocket_Equation/code.py
+++ Day_1_The_Tyranny_of_the_Rocket_Equation/code.py
@@ -1,9 +1,25 @@
-# part 1
-result = 0
+#------part 1------#
+result1 = 0
 with open('input.txt', 'r') as f:
 	for line in f.readlines():
 		data = int(line.strip())
 		fuel = int(data/3)
 		fuel -= 2
-		result += fuel
-print(result)
+		result1 += fuel
+print(result1)
+
+#------part 2------#
+
+result2 = 0
+with open('input.txt', 'r') as f:
+	for line in f.readlines():
+		data = int(line.strip())
+		fuel = int(data/3)
+		fuel -= 2
+		result2 += fuel
+		while (int(fuel/3) - 2) >= 0:
+			subfuel = int(fuel /3) - 2
+			result2 += subfuel
+			fuel = subfuel
+			print(subfuel)
+print(result2)
