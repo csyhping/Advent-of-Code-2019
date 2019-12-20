86ea7fc4340619ddd732c070c4ad518133960fed
csyhping <csyhping@connect.hku.hk>
2019-12-20 17:38:38 +0800

    Day 6 part 1

--- Day_6_Universal_Orbit_Map/code_part1.py
+++ Day_6_Universal_Orbit_Map/code_part1.py
@@ -1,22 +1,23 @@
 import numpy as np 
-
+from tqdm import tqdm as tqdm 
 def countOrbits(all_orbits, curr_planet, curr_count):
+	# print('checking ===', curr_planet)
 	# count orbits for each planet
 	for orbit in all_orbits:
 		orbb = orbit.split(')')
 		p1, p2 = orbb[0], orbb[1]
-		print(orbb)
+		# print(orbb)
 		if p2 == curr_planet:
-			if p1 != 'A':
+			if p1 != 'COM':
 				curr_count += 1
 				subcount = countOrbits(all_orbits, p1, 0)
-				return subcount
+				return subcount + curr_count
 			else:
 				curr_count += 1
 	# counts = 0
 	return curr_count
 
-orbits = np.loadtxt('case.txt', dtype = str)
+orbits = np.loadtxt('input.txt', dtype = str)
 planets = []
 
 # get all planets 
@@ -29,10 +30,14 @@ for orbit in orbits:
 planets = set(planets)
 
 # delete COM
-planets.remove('A')
-
+planets.remove('COM')
+print(planets)
 orbitcount = 0
 # count indirect and direct orbits for each planet
-for planet in planets:
+for planet in tqdm(planets):
+	# print('current plane = ', planet)
 	sub_count = countOrbits(orbits, planet, 0)
+	# print(sub_count)
 	orbitcount += sub_count
+
+print(orbitcount)
